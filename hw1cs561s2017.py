910806c51e88470a1926957976c5500b9d720933
ZonghanChang <changzonghan@gmail.com>
2017-02-01 16:18:41 -0800

    implement getvalidaction

--- hw1cs561s2017.py
+++ hw1cs561s2017.py
@@ -8,6 +8,10 @@ weights = [[99, -8, 8, 6, 6, 8, -8, 99],
            [-8, -24, -4, -3, -3, -4, -24, -8],
            [99, -8, 8, 6, 6, 8, -8, 99]]
 
+row_name = [1, 2, 3, 4, 5, 6, 7, 8]
+column_name = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+directions = ((0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1))
+blank = '*'
 def readFile(filepath):
     file = open(filepath)
     player = file.readline().strip('\n')
@@ -25,13 +29,36 @@ def findmax(board, depth, player, alpha, beta):
     pass
 
 def getvalidaction(board, player):
-    pass
+    opponent = 'O' if player == 'X' else 'X'
+    valid_pos = list()
+    for i in range(0, len(board)):
+        for j in range(0, len(board[0])):
+            if board[i][j] == blank:
+                cur = [i, j]
+                for direction in directions:
+                    while True:
+                        next = [direction[0] + cur[0], direction[1] + cur[1]]
+                        if next[0] < 0 or next[1] < 0 or next[0] >= len(board) or next[1] >= len(board[0]):
+                            break
+                        if board[next[0]][next[1]] == player and cur == [i, j]:
+                            break
+                        if board[next[0]][next[1]] == blank:
+                            break
+                        if board[next[0]][next[1]] == player:
+                            valid_pos.append((i, j))
+                        cur = next
+
+    return valid_pos
 
 def calvalue(board, player):
     value = 0
     for i in range(0, len(board)):
         for j in range(0, len(board[0])):
-            if board[i][j] == player:
+            if board[i][j] == '*':
+                pass
+            elif board[i][j] == player:
                 value += weights[i][j]
+            else:
+                value -= weights[i][j]
     return value
 
