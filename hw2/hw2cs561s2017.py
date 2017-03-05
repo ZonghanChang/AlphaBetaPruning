18adac5a5edbe3936cc853f1600d5875a3ac0437
ZonghanChang <changzonghan@gmail.com>
2017-03-05 01:37:09 -0800

    hw2 back up

--- hw2/hw2cs561s2017.py
+++ hw2/hw2cs561s2017.py
@@ -2,15 +2,11 @@
 
 from copy import deepcopy
 
-a = dict()
-symbol = 'b'
-a[symbol] = 1
-print a
-
 
 class Clause:
-    positive_symbol = set()
-    negtive_symbol = set()
+    def __init__(self):
+        self.positive_symbol = set()
+        self.negtive_symbol = set()
 
     def add_positive(self, symbol):
         self.positive_symbol.add(symbol)
@@ -18,10 +14,19 @@ class Clause:
     def add_negtive(self, symbol):
         self.negtive_symbol.add(symbol)
 
+    def get_positive(self):
+        return self.positive_symbol
+
+    def get_negtive(self):
+        return self.negtive_symbol
+
+
 class Model:
-    assigments = dict()
+    def __init__(self):
+        self.assigments = dict()
 
     def __init__(self, values):
+        self.assigments = dict()
         self.assigments.update(values)
 
     def union(self, symbol):
@@ -35,39 +40,92 @@ class Model:
         return self
 
     def satisfies(self, clauses):
-        for c in clauses:
-            pass
+        for clause in clauses:
+            value = self.determin_value(clause)
+            if value is None or value is False :
+                return False
+
+        return True
 
     def determin_value(self, clause):
-        for item in clause:
-            if item in self.assigments:
-                return True
 
-def every_clause_true():
-    pass
-def some_clause_false():
-    pass
+        for positive in clause.get_positive():
+            if positive in self.assigments:
+                value = self.assigments[positive]
+                if value is True:
+                    return True
+
+        for negtive in clause.get_negtive():
+            if negtive in self.assigments:
+                value = self.assigments[negtive]
+                if value is False:
+                    return True
+
+        return None
+
+
+def every_clause_true(clauses, model):
+    return model.satisfies(clauses)
+
+
+def some_clause_false(clauses, model):
+    for clause in clauses:
+        if model.determin_value(clause) is False:
+            return True
+    return False
+
+def find_pure_symbol(symbols, clauses, model):
+    symbols_to_keep = deepcopy(symbols)
+    candidate_pure_positive_symbols = set()
+    candidate_pure_negtive_symbols = set()
+    for clause in clauses:
+        if model.determin_value(clause) is True:
+            continue
+        for p in clause.get_positive():
+            if p in symbols_to_keep:
+                candidate_pure_positive_symbols.add(p)
+        for n in clause.get_negtive():
+            if n in symbols_to_keep:
+                candidate_pure_negtive_symbols.add(n)
+
+    intersect = candidate_pure_positive_symbols & candidate_pure_negtive_symbols
+    candidate_pure_positive_symbols -= intersect
+    candidate_pure_negtive_symbols -= intersect
+
+    if len(candidate_pure_positive_symbols) != 0:
+        return candidate_pure_positive_symbols.
+
+
 
 def dpll(clauses, symbols, model):
     if every_clause_true(clauses, model):
         return True
     if some_clause_false(clauses, model):
         return False
+    if len(symbols) == 0:
+        return False
+    p = symbols[0]
+    rest = symbols[1:]
+    return dpll(clauses, rest, model.union_inplace(p, True)) or dpll(clauses, rest, model.union_inplace(p, False))
 
 
-file = open("input.txt", "r")
+file = open("input4.txt", "r")
 first_line = file.readline().strip('\r\n').split()
 guests = int(first_line[0])
 tables = int(first_line[1])
 sentence = list()
+symbols = list()
 for i in range(guests):
-    in_table_clause = set()
+    in_table_clause = Clause()
     for j in range(tables):
-        in_table_clause.add((True, (i + 1, j + 1)))
+        in_table_clause.add_positive((i + 1, j + 1))
+        symbols.append((i + 1, j + 1))
         for k in range(j + 1, tables):
-            in_onetable_clause = set()
-            in_onetable_clause.add((False, (i + 1, j + 1)))
-            in_onetable_clause.add((False, (i + 1, k + 1)))
+            in_onetable_clause = Clause()
+            in_onetable_clause.add_negtive((i + 1, j + 1))
+            in_onetable_clause.add_negtive((i + 1, k + 1))
+            symbols.append((i + 1, j + 1))
+            symbols.append((i + 1, k + 1))
             sentence.append(in_onetable_clause)
     sentence.append(in_table_clause)
 
@@ -80,9 +138,11 @@ for line in file:
         a = int(tokens[0])
         b = int(tokens[1])
         for i in range(tables):
-            enemy_clause = set()
-            enemy_clause.add((False, (a, i + 1)))
-            enemy_clause.add((False, (b, i + 1)))
+            enemy_clause = Clause()
+            enemy_clause.add_negtive((a, i + 1))
+            enemy_clause.add_negtive((b, i + 1))
+            symbols.append((a, i + 1))
+            symbols.append((b, i + 1))
             sentence.append(enemy_clause)
     elif tokens[-1] == 'F':
         a = int(tokens[0])
@@ -90,9 +150,12 @@ for line in file:
         for i in range(tables):
             for j in range(tables):
                 if i != j:
-                    friend_clause = set()
-                    friend_clause.add((False, (a, i + 1)))
-                    friend_clause.add((False, (b, j + 1)))
+                    friend_clause = Clause()
+                    friend_clause.add_negtive((a, i + 1))
+                    friend_clause.add_negtive((b, j + 1))
+                    symbols.append((a, i + 1))
+                    symbols.append((b, j + 1))
                     sentence.append(friend_clause)
 
-print sentence
+model = Model(dict())
+print dpll(sentence, symbols, model)
