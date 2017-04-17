7cccf30a0d671ebc40eab12c60dfdbdb471f8a3d
ZonghanChang <changzonghan@gmail.com>
2017-04-17 03:14:24 -0700

    fix bug when calculating p

--- hw3/hw3cs561s2017.py
+++ hw3/hw3cs561s2017.py
@@ -1,11 +1,61 @@
 #!/usr/bin/python
 import re
 
+class Graph:
+    def __init__(self):
+        self.nodes = dict()
+        self.variablelist = list()
+
+    def getnode(self, name):
+        return self.nodes[name]
+
+    def getvariables(self):
+        return self.variablelist
+
+    def addutility(self):
+        pass
+
+    def addnode(self, block):
+        s = block[0]
+        node = Node()
+        nodename = ""
+        if s.find("|") != -1:
+
+            edge = s.split("|")
+            nodename = edge[0].strip()
+            parentnodeslist = edge[1].strip().split(" ")
+            for parentnode in parentnodeslist:
+                node.addparent(parentnode)
+            parentnodesset = set(parentnodeslist)
+
+            for line in block[1:]:
+                tokens = line.strip().split(" ")
+                p = tokens[0]
+                positivelist = list()
+                for i in range(1, len(tokens)):
+                    if tokens[i] == "+":
+                        positivelist.append(parentnodeslist[i - 1])
+                node.addprobability(tuple(positivelist), p)
+            self.nodes[nodename] = node
+            self.variablelist.append(nodename)
+        else:
+            nodename = s
+            if block[1].find("decision") != -1:
+                node.addprobability(tuple(), 1)
+                node.isutility = True
+            else:
+                p = block[1].strip()
+                node.addprobability(tuple(), p)
+            self.nodes[nodename] = node
+            self.variablelist.append(s)
+
 class Node:
     def __init__(self):
         self.parents = list()
         self.probability = dict()
         self.utility = dict()
+        self.isutility = False
+
     def addutility(self, parent, utility):
         self.utility[parent] = utility
 
@@ -16,7 +66,13 @@ class Node:
         self.probability[parent] = probability
 
     def getprobability(self, parent):
-        return self.probability[parent]
+        return float(self.probability[parent])
+
+    def getprobabilitybar(self, parent):
+        if self.isutility:
+            return 1
+        else:
+            return 1 - float(self.probability[parent])
 
     def addparent(self, parent):
         self.parents.append(parent)
@@ -87,9 +143,9 @@ def enumeration_ask(queryvariables, observedvariables, bn):
             o1[k] = v
         p = enumeration_all(allvaribles, o1, bn)
         if cmp(condition, queryvariables) == 0:
-            pquery += p
+            pquery = p
         distribution.append(p)
-    return pquery * 1.0 / sum(distribution)
+    return pquery * (1.0 / sum(distribution))
 
 def enumeration_all(vars, observedvariables, bn):
     if len(vars) == 0:
@@ -103,18 +159,19 @@ def enumeration_all(vars, observedvariables, bn):
 
     if Yname in observedvariables:
         if observedvariables[Yname]:
-            p = float(Ynode.getprobability(tuple(templist))) * enumeration_all(vars, observedvariables, bn)
+            p = Ynode.getprobability(tuple(templist)) * enumeration_all(vars, observedvariables, bn)
             vars.append(Yname)
             return p
         else:
-            p = (1 - float(Ynode.getprobability(tuple(templist)))) * enumeration_all(vars, observedvariables, bn)
+            p = Ynode.getprobabilitybar(tuple(templist)) * enumeration_all(vars, observedvariables, bn)
             vars.append(Yname)
             return p
     else:
         observedvariables[Yname] = True
-        ptrue = float(Ynode.getprobability(tuple(templist))) * enumeration_all(vars, observedvariables, bn)
+        ptrue = Ynode.getprobability(tuple(templist)) * enumeration_all(vars, observedvariables, bn)
         observedvariables[Yname] = False
-        pfalse = (1 - float(Ynode.getprobability(tuple(templist)))) * enumeration_all(vars, observedvariables, bn)
+        pfalse = Ynode.getprobabilitybar(tuple(templist)) * enumeration_all(vars, observedvariables, bn)
+        del observedvariables[Yname]
         vars.append(Yname)
         p = ptrue + pfalse
         return p
@@ -146,6 +203,7 @@ def readfile():
         graph.addnode(block)
 
     line = file.readline().strip("\n\r")
+
     if line:
         utilityparent = line.split("|")[1]
         p = re.compile(r' +')
@@ -157,55 +215,7 @@ def readfile():
                 break
             tokens = p.split(line.strip())
 
-
     return querys, graph
 
-
-class Graph:
-    def __init__(self):
-        self.nodes = dict()
-        self.variablelist = list()
-
-    def getnode(self, name):
-        return self.nodes[name]
-
-    def getvariables(self):
-        return self.variablelist
-
-    def addutility(self):
-        pass
-
-    def addnode(self, block):
-        s = block[0]
-        node = Node()
-        nodename = ""
-        if s.find("|") != -1:
-
-            edge = s.split("|")
-            nodename = edge[0].strip()
-            parentnodeslist = edge[1].strip().split(" ")
-            for parentnode in parentnodeslist:
-                node.addparent(parentnode)
-            parentnodesset = set(parentnodeslist)
-
-            for line in block[1:]:
-                tokens = line.strip().split(" ")
-                p = tokens[0]
-                positivelist = list()
-                for i in range(1, len(tokens)):
-                    if tokens[i] == "+":
-                        positivelist.append(parentnodeslist[i - 1])
-                node.addprobability(tuple(positivelist), p)
-            self.nodes[nodename] = node
-            self.variablelist.append(nodename)
-        else:
-            nodename = s
-            if block[1].find("decision") != -1:
-                node.addprobability(tuple(), 1)
-            else:
-                p = block[1].strip()
-                node.addprobability(tuple(), p)
-            self.nodes[nodename] = node
-            self.variablelist.append(s)
 if __name__ == '__main__':
     main()
